fde7718bc5e32c36e22ff21a34083981334c9d58
Raed Baker <raed.baker@qumal.com>
2023-09-05 02:00:32 +0300

    Initial commit

---
+++ musicplayer/scripts/songs.js
@@ -0,0 +1,20 @@
+let songs = [
+  {
+    name: "Quran",
+    cover:
+      "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+    artist: "Hakim Omari",
+    audio: "/Users/raedbaker/Downloads/test123.mp3", // Use local file path
+    id: 0,
+  },
+  // Add other songs with local file paths
+  {
+    name: "Quran1",
+    cover:
+      "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+    artist: "Hakim Omari1",
+    audio: "/Users/raedbaker/Downloads/test123.mp3", // Use local file path
+    id: 1,
+  },
+  // Add more songs here
+];
